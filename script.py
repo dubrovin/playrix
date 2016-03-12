482f2b470095f98bf7af2dad29e9a5b4816676aa
Dubrovin Ruslan <dubrovin.ruslan@gmail.com>
2016-03-12 20:37:15 +0300

    100% done

--- script.py
+++ script.py
@@ -1,8 +1,9 @@
 # -*- coding: utf-8 -*-
-import csv
-import os
-import shutil
-import string
+from csv import QUOTE_MINIMAL, writer as csv_writer
+from os import makedirs, path, listdir
+from shutil import rmtree
+from string import ascii_lowercase
+from time import clock
 
 from multiprocessing import Process
 from uuid import uuid4
@@ -10,21 +11,9 @@ from random import randrange, choice
 from xml.etree.ElementTree import ElementTree, Element, SubElement, fromstring
 from zipfile import ZipFile, is_zipfile
 
-# 1. Создает 50 zip-архивов, в каждом 100 xml файлов со случайными данными следующей структуры:
-# <root>
-#     <var name=’id’ value=’<случайное уникальное строковое значение>’/>
-#     <var name=’level’ value=’<случайное число от 1 до 100>’/>
-#     <objects>
-#         <object name=’<случайное строковое значение>’/>
-#         <object name=’<случайное строковое значение>’/>
-#         …
-#     </objects>
-# </root>
-
-# В тэге objects случайное число (от 1 до 10) вложенных тэгов object.
-
+#first part
 def get_random_word(length):
-   return ''.join(choice(string.ascii_lowercase) for i in range(length))
+   return ''.join(choice(ascii_lowercase) for i in range(length))
 
 def create_xml_tree():
     root_tag = Element('root')
@@ -44,34 +33,25 @@ def create_xml_tree():
 
 def generate():
     tmp_dir = 'tmp'
-    if not os.path.exists(tmp_dir):
-        os.makedirs(tmp_dir)
+    if not path.exists(tmp_dir):
+        makedirs(tmp_dir)
     for i in range(0, 50):
         # create zip
         with ZipFile(''.join([str(i), '.zip']), 'w') as zip_file:
             for j in range(0, 100):
                 xml_tree = create_xml_tree()
                 xml_file_name = ''.join([str(j), '.xml'])
-                xml_file_path = os.path.join(tmp_dir, xml_file_name)
+                xml_file_path = path.join(tmp_dir, xml_file_name)
                 xml_tree.write(xml_file_path)
                 zip_file.write(xml_file_path, xml_file_name)
-    shutil.rmtree(tmp_dir)
+    rmtree(tmp_dir)
 
-# 2. Обрабатывает директорию с полученными zip архивами, разбирает вложенные xml файлы и формирует 2 csv файла:
-# Первый: id, level - по одной строке на каждый xml файл
-# Второй: id, object_name - по отдельной строке для каждого тэга object (получится от 1 до 10 строк на каждый xml файл)
-
-# Очень желательно сделать так, чтобы задание 2 эффективно использовало ресурсы многоядерного процессора.
-# Также желательно чтобы программа работала быстро.
-
+#second part
 def parse_1st_csv(zip_file):
-    file_name = ''.join(zip_file.filename.split('.')[0])
-    with open(
-            ''.join(['1st_', file_name, '.csv']), 'w', newline=''
-        ) as csv_file:
-        writer = csv.writer(
+    with open('1st.csv', 'a', newline='') as csv_file:
+        writer = csv_writer(
                 csv_file, delimiter=' ',
-                quotechar='|', quoting=csv.QUOTE_MINIMAL
+                quotechar='|', quoting=QUOTE_MINIMAL
         )
         for name in zip_file.namelist():
             xml_tree = fromstring(zip_file.read(name))
@@ -80,29 +60,45 @@ def parse_1st_csv(zip_file):
                 xml_tree.find('level').text,
             ])
 
-
+def parse_2nd_csv(zip_file):
+    with open('2nd.csv', 'a', newline='') as csv_file:
+        writer = csv_writer(
+                csv_file, delimiter=' ',
+                quotechar='|', quoting=QUOTE_MINIMAL
+        )
+        for name in zip_file.namelist():
+            xml_tree = fromstring(zip_file.read(name))
+            id_text = xml_tree.find('id').text
+            for child in xml_tree.find('objects').getchildren():
+                writer.writerow([
+                    id_text,
+                    child.get('name'),
+                ])
 
 def parse():
     jobs = []
-    for file in os.listdir():
+    for file in listdir():
         if is_zipfile(file):
-            with ZipFile(file) as zip_file:
-                # take file name without extension
-                tmp_dir = ''.join(zip_file.file_name.split('.')[0])
-                # zip_file.extractall(tmp_dir)
-                jobs.append(Process(
-                    target=parse_1st_csv,
-                    args=(zip_file)
-                    ))
-                # for name in zip_file.name_list():
-                #     xml_file = os.path.join(tmp_dir, name)
-                #     xml_tree = parse(xml_file)
+            zip_file = ZipFile(file)
+            j1 = Process(target=parse_1st_csv, args=(zip_file,))
+            j2 = Process(target=parse_2nd_csv, args=(zip_file,))
 
+            jobs.extend([
+                j1, j2
+            ])
 
+            j1.start()
+            j2.start()
+    for job in jobs:
+        job.join()
 
-                shutil.rmtree(tmp_dir)
-
-
+if __name__ == '__main__':
+    print('starting')
+    start = clock()
+    generate()
+    parse()
+    end = clock()
+    print('ending \nworking time: ', end - start)
 
 
 
